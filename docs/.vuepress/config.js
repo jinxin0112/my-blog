00f740a93433b5ab1a37736f8f4efe46018a5aa0
jinxin <m18508218948@163.com>
2018-10-08 09:39:57 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -45,6 +45,7 @@ module.exports = {
                 children: [
                     '',
                     'zoom',
+                    'jyzhp',
                     'sql'
                 ]
             }],
