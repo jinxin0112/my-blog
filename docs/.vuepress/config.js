7bbc443d56328228467ef5f4c183356e88e682a4
jinxin <m18508218948@163.com>
2019-03-09 18:47:50 +0800

    chore: 移除无用文件

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -7,73 +7,23 @@ module.exports = {
 
     themeConfig: {
         nav: [
-            {
-                text: 'Blog',
-                items: [
-                    { text: 'JavaScript', link: '/Blog/javascript/' },
-                    { text: 'CSS', link: '/Blog/css/' },
-                    { text: 'Life', link: '/Blog/life/' },
-                    { text: 'Orther', link: '/Blog/orther/' }
-                ]
-            },
-            { text: 'Project', link: '/Project/' },
-            { text: 'Resume', link: '/Resume/' },
-            { text: 'Interview', link: '/Interview/' },
             { text: 'GitHub', link: 'https://github.com/kingDuiDui/my-blog' }
         ],
         sidebar: {
             '/Blog/javascript/': [{
-                title: 'JavaScript',
+                title: 'my blog',
                 collapsable: false,
                 children: [
                     '',
-                    'http',
                     'stream',
                     'prototype',
                     'eventLoop',
-                    'promise'
-                ]
-            }],
-            '/Blog/css/': [{
-                title: 'CSS',
-                collapsable: false,
-                children: ['/']
-            }],
-            '/Blog/orther/': [{
-                title: 'Orther',
-                collapsable: false,
-                children: [
-                    '',
+                    'promise',
+                    'morethancode',
                     'zoom',
-                    'jyzhp',
-                    'sql'
+                    'jyzhp'
                 ]
             }],
-            '/Project/': [{
-                title: 'Project',
-                collapsable: false,
-                children: [
-                    '',
-                    'search'
-                ]
-            }],
-            '/Interview/': [{
-                title: 'Interview',
-                collapsable: false,
-                children: [
-                    '',
-                    'Interview',
-                    'flex',
-                    'mvvm',
-                    'color',
-                    'router',
-                    'debounce',
-                    'shareCookie',
-                    'event',
-                    'xss',
-                    'maxArr',
-                ]
-            }]
         }
     }
 }
