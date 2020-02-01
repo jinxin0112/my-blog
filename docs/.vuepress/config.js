d45be8722fe417414f16d0d9e6c1426bebe29ee1
jinxin0112 <m18508218948@163.com>
2020-02-01 19:38:57 +0800

    chore(config): add friendLink

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -38,7 +38,21 @@ module.exports = {
             owner: 'jinxin0112',
             repo: 'my-blog',
             clientId: 'fadc75f7996bfcf8e2eb',
-            clientSecret: '51293ed3b9d548d532577c0b9eb7b2148e26c2a0',
-          }
+            clientSecret: '51293ed3b9d548d532577c0b9eb7b2148e26c2a0'
+        },
+        friendLink: [
+            {
+                title: 'LeonGravel',
+                desc: '一个脱离了高级趣味的空栈工程师',
+                logo: 'https://s.gravatar.com/avatar/fd0e02edb15e0dfd419fba201edebfeb?s=80',
+                link: 'https://leongravel.com/'
+            },
+            {
+                title: 'JordanZhang',
+                desc: 'The only thing I can keep doing, just lazy coding!',
+                logo: 'http://jordanzhang.xyz/assets/blogImg/lufei.jpg',
+                link: 'http://jordanzhang.xyz/'
+            }
+        ]
     }
 };
